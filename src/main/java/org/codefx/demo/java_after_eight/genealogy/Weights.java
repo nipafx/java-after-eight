5c020a8e5fcbba1e5e7b943c91c86bd35d60fade
Nicolai Parlog <nipa@codefx.org>
2020-01-30 12:10:52 +0100

    Implement genealogy

---
+++ src/main/java/org/codefx/demo/java_after_eight/genealogy/Weights.java
@@ -0,0 +1,27 @@
+package org.codefx.demo.java_after_eight.genealogy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class Weights {
+
+	private final Map<RelationType, Double> weights;
+	private final double defaultWeight;
+
+	private Weights(Map<RelationType, Double> weights, double defaultWeight) {
+		// REFACTOR 10: Map::copyOf
+		this.weights = new HashMap<>(weights);
+		if (this.weights.entrySet().stream().anyMatch(entry -> entry.getKey() == null || entry.getValue() == null))
+			throw new NullPointerException("Neither relation type nor weight can be null.");
+		this.defaultWeight = defaultWeight;
+	}
+
+	public static Weights from(Map<RelationType, Double> weights, double defaultWeight) {
+		return new Weights(weights, defaultWeight);
+	}
+
+	public double weightOf(RelationType genealogistType) {
+		return weights.getOrDefault(genealogistType, defaultWeight);
+	}
+
+}
