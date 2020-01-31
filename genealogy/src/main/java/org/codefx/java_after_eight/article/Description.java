nipafx/java-after-eight

commit 8c33d015a600a39c07d2b838f8f7857d374d3f1a

Better distribute checks between constructors and static factories

genealogy/src/main/java/org/codefx/java_after_eight/article/Description.java
@@ -4,19 +4,20 @@ import java.util.Objects;
 
 import static java.util.Objects.requireNonNull;
 
-// REFACTOR 14: records
 public class Description {
 
 	private final String text;
 
 	private Description(String text) {
-		this.text = requireNonNull(text);
-		if (text.isEmpty())
-			throw new IllegalArgumentException("Description can't have an empty text.");
+		this.text = text;
 	}
 
 	static Description from(String text) {
-		return new Description(Utils.removeOuterQuotationMarks(text));
+		requireNonNull(text);
+		String unquotedText = Utils.removeOuterQuotationMarks(text).trim();
+		if (unquotedText.isEmpty())
+			throw new IllegalArgumentException("Description can't have an empty text.");
+		return new Description(text);
 	}
 
 	public String text() {

genealogy/src/main/java/org/codefx/java_after_eight/article/Slug.java
@@ -4,18 +4,18 @@ import java.util.Objects;
 
 import static java.util.Objects.requireNonNull;
 
-// REFACTOR 14: records
 public class Slug implements Comparable<Slug> {
 
 	private final String value;
 
 	private Slug(String value) {
-		this.value = requireNonNull(value);
-		if (value.isEmpty())
-			throw new IllegalArgumentException("Slugs can't have an empty value.");
+		this.value = value;
 	}
 
 	static Slug from(String value) {
+		requireNonNull(value);
+		if (value.isEmpty())
+			throw new IllegalArgumentException("Slugs can't have an empty value.");
 		return new Slug(value);
 	}
 

genealogy/src/main/java/org/codefx/java_after_eight/article/Title.java
@@ -4,19 +4,20 @@ import java.util.Objects;
 
 import static java.util.Objects.requireNonNull;
 
-// REFACTOR 14: records
 public class Title {
 
 	private final String text;
 
 	private Title(String text) {
-		this.text = requireNonNull(text);
-		if (text.isEmpty())
-			throw new IllegalArgumentException("Titles can't have an empty text.");
+		this.text = text;
 	}
 
 	static Title from(String text) {
-		return new Title(Utils.removeOuterQuotationMarks(text));
+		requireNonNull(text);
+		String unquotedText = Utils.removeOuterQuotationMarks(text);
+		if (unquotedText.isEmpty())
+			throw new IllegalArgumentException("Titles can't have an empty text.");
+		return new Title(unquotedText);
 	}
 
 	public String text() {

genealogy/src/main/java/org/codefx/java_after_eight/genealogy/RelationType.java
@@ -13,12 +13,13 @@ public class RelationType {
 	private final String value;
 
 	private RelationType(String value) {
-		this.value = requireNonNull(value);
-		if (value.isEmpty())
-			throw new IllegalArgumentException("Relation types can't have an empty value.");
+		this.value = value;
 	}
 
 	public static RelationType from(String value) {
+		requireNonNull(value);
+		if (value.isEmpty())
+			throw new IllegalArgumentException("Relation types can't have an empty value.");
 		return new RelationType(value);
 	}
 

genealogy/src/main/java/org/codefx/java_after_eight/genealogy/TypedRelation.java
@@ -15,17 +15,20 @@ public class TypedRelation {
 	private final long score;
 
 	private TypedRelation(Article article1, Article article2, RelationType type, long score) {
-		this.article1 = requireNonNull(article1);
-		this.article2 = requireNonNull(article2);
-		this.type = requireNonNull(type);
-		this.score = requireNonNull(score);
-
-		if (score < 0 || 100 < score)
-			throw new IllegalArgumentException("Score should be in interval [0; 100]: " + toString());
+		this.article1 = article1;
+		this.article2 = article2;
+		this.type = type;
+		this.score = score;
 	}
 
 	public static TypedRelation from(Article article1, Article article2, RelationType type, long score) {
-		return new TypedRelation(article1, article2, type, score);
+		if (score < 0 || 100 < score)
+			throw new IllegalArgumentException("Score should be in interval [0; 100]: " + score);
+		return new TypedRelation(
+				requireNonNull(article1),
+				requireNonNull(article2),
+				requireNonNull(type),
+				score);
 	}
 
 	public Article article1() {

genealogy/src/main/java/org/codefx/java_after_eight/recommendation/Recommendation.java
@@ -23,7 +23,7 @@ public class Recommendation {
 
 	static Recommendation from(Article article, Stream<Article> sortedRecommendations, int perArticle) {
 		List<Article> recommendations = sortedRecommendations.limit(perArticle).collect(toList());
-		return new Recommendation(article, recommendations);
+		return new Recommendation(requireNonNull(article), recommendations);
 	}
 
 	public Article article() {

commit 853d629965daafc478afd0575a1f7cb18c8a88cb

Fix bug introduced by 8c33d01 (Note: tests help more if executed)

genealogy/src/main/java/org/codefx/java_after_eight/article/Description.java
@@ -17,7 +17,7 @@ public class Description {
 		String unquotedText = Utils.removeOuterQuotationMarks(text).trim();
 		if (unquotedText.isEmpty())
 			throw new IllegalArgumentException("Description can't have an empty text.");
-		return new Description(text);
+		return new Description(unquotedText);
 	}
 
 	public String text() {