e125f282900f3b270e1b68ffee15be94e08bf563
Nicolai Parlog <nipa@codefx.org>
2020-01-31 23:45:58 +0100

    Use version API

--- genealogy/src/main/java/module-info.java
+++ genealogy/src/main/java/module-info.java
@@ -1,8 +1,6 @@
 import org.codefx.java_after_eight.genealogist.GenealogistService;
 
 module org.codefx.java_after_eight.genealogy {
-	requires java.management;
-
 	exports org.codefx.java_after_eight.post;
 	exports org.codefx.java_after_eight.genealogist;
 
--- genealogy/src/main/java/org/codefx/java_after_eight/ProcessDetails.java
+++ genealogy/src/main/java/org/codefx/java_after_eight/ProcessDetails.java
@@ -1,7 +1,5 @@
 package org.codefx.java_after_eight;
 
-import java.util.Optional;
-
 import static java.lang.String.format;
 
 public class ProcessDetails {
@@ -10,23 +8,7 @@ public class ProcessDetails {
 		return format(
 				"Process ID: %s | Major Java version: %s",
 				ProcessHandle.current().pid(),
-				getMajorJavaVersion().map(Object::toString).orElse("unknown"));
-	}
-
-	public static Optional<Integer> getMajorJavaVersion() {
-		try {
-			String version = System.getProperty("java.version");
-			if (version.startsWith("1."))
-				return Optional.of(Integer.parseInt(version.substring(2, 3)));
-
-			if (version.contains("."))
-				return Optional.of(Integer.parseInt(version.split("\\.")[0]));
-
-			// hail mary
-			return Optional.of(Integer.parseInt(version));
-		} catch (NumberFormatException ex) {
-			return Optional.empty();
-		}
+				Runtime.version().major());
 	}
 
 }
