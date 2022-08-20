22efccced370af4b643ad82c1ca480a5ef137b77
Nicolai Parlog <nipa@codefx.org>
2022-08-20 11:58:03 +0200

    Configure JavaDoc

--- genealogy/src/main/java/org/codefx/java_after_eight/genealogist/GenealogistService.java
+++ genealogy/src/main/java/org/codefx/java_after_eight/genealogist/GenealogistService.java
@@ -6,6 +6,15 @@ import java.util.Collection;
 
 /**
  * Used as a service to create {@link Genealogist}s - must have a public parameterless constructor.
+ *
+ * <p>Intended use:
+ * <pre>
+ * {@code
+ * Collection<Post> posts = // ... ;
+ * GenealogistService service = new SpecificGenealogistService();
+ * Genealogist genealogist = service.create(posts);
+ * }
+ * </pre>
  */
 public interface GenealogistService {
 
